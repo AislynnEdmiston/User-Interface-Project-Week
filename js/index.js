ca85758044960920af4ee2a35249f17d5174b11e
Aislynn E <davidandaislynn@gmail.com>
2019-03-12 01:09:41 -0400

    Finished styling home page, finished making the js for the nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // JS goes here
+
+function toggleNavOpen(){
+    navOpen.classList.toggle('hidden');
+    console.log('click');
+}
+
+const navOpen = document.querySelector('.navContainerOpen');
+const closeBtn = document.querySelector('.closed');
+const openBtn = document.querySelector('.open');
+
+closeBtn.addEventListener('click', () => {
+    toggleNavOpen();
+});
+openBtn.addEventListener('click', () => {
+    toggleNavOpen();
+});
